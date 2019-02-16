99a9d61263f5180936fab4a17d88b25de8f99c55
askar <ainash.pulatovap@mail.ru>
2019-02-16 14:23:56 +0600

    upd

---
+++ week1/task2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()); // создаю инт и ввожу значение
+            string line = Console.ReadLine();//записываю в строку
+            string[] arr = line.Split();//из строки записываю в массив
+            string[] arr1 = new string[2 * n];//создаю новый массив в два раза больше
+            for (int j = 0; j < arr.Length; j++)//цикл от начала до конца длины массива
+            {
+                arr1[2 * j] = arr1[2 * j + 1] = arr[j];//записываю два раза элементы 
+            }
+            for (int j = 0; j < arr1.Length; j++)//цикл от начала до конца длины массива
+            {
+                Console.Write(arr1[j] + " ");//вывод элементов нового массива
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ week1/task3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());//ввожу число в виде строки и сразу переводим в int
+            for (int i = 1; i <= n; i++)//цикл, который проходит с 1 до n
+            {
+                for (int j = 0; j < i; j++)//цикл, который проходит с 0 до i
+                {
+
+                    Console.Write("[*]");//вывожу "[*]"
+
+
+                }
+                Console.WriteLine("");//вывожу "" и перехожу на следующую строку
+            }
+            Console.ReadKey();
+        }
+    }
+}
